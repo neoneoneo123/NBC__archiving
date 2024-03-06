3e2bd1dcc1a4aa31345a33c1905f1b9acb35688d
neo <gouxrunner@gmail.com>
2024-03-06 18:23:07 +0900

    Lv2

--- NBC__calculator/app/src/main/java/com/example/nbc__calculator/Calculator.kt
+++ NBC__calculator/app/src/main/java/com/example/nbc__calculator/Calculator.kt
@@ -10,49 +10,61 @@ class Calculator {
 
     fun addition () {
         println("더하기를 선택하셨습니다.")
-        println("좌항을 입력해주세요.")
+        println("[좌] + [ ] 입력해주세요.")
         this.left = readLine()!!.toDouble()
-        println("우항을 입력해주세요.")
+        println("[ ] + [우] 입력해주세요.")
         this.right = readLine()!!.toDouble()
 
-        println("======== 계산 결과 =========")
+        println("=========== 계산 결과 ============")
         println("${this.left} + ${this.right} = ${left + right}")
         println("")
     }
 
     fun subtraction () {
         println("빼기를 선택하셨습니다.")
-        println("좌항을 입력해주세요.")
+        println("[좌] - [ ] 입력해주세요.")
         this.left = readLine()!!.toDouble()
-        println("우항을 입력해주세요.")
+        println("[ ] - [우] 입력해주세요.")
         this.right = readLine()!!.toDouble()
 
-        println("======== 계산 결과 =========")
+        println("=========== 계산 결과 ============")
         println("${this.left} - ${this.right} = ${left - right}")
         println("")
     }
 
     fun multiplication () {
         println("곱하기를 선택하셨습니다.")
-        println("좌항을 입력해주세요.")
+        println("[좌] * [ ] 입력해주세요.")
         this.left = readLine()!!.toDouble()
-        println("우항을 입력해주세요.")
+        println("[ ] * [우] 입력해주세요.")
         this.right = readLine()!!.toDouble()
 
-        println("======== 계산 결과 =========")
+        println("=========== 계산 결과 ============")
         println("${this.left} * ${this.right} = ${left * right}")
         println("")
     }
 
     fun division () {
         println("나누기를 선택하셨습니다.")
-        println("좌항을 입력해주세요.")
+        println("[좌] / [ ] 입력해주세요.")
         this.left = readLine()!!.toDouble()
-        println("우항을 입력해주세요.")
+        println("[ ] / [우] 입력해주세요.")
         this.right = readLine()!!.toDouble()
 
-        println("======== 계산 결과 =========")
+        println("=========== 계산 결과 ============")
         println("${this.left} / ${this.right} = ${left / right}")
+        println("")
+    }
+
+    fun mod () {
+        println("나머지를 선택하셨습니다.")
+        println("[좌] % [ ] 입력해주세요.")
+        this.left = readLine()!!.toDouble()
+        println("[ ] % [우] 입력해주세요.")
+        this.right = readLine()!!.toDouble()
+
+        println("=========== 계산 결과 ============")
+        println("${this.left} % ${this.right} = ${left % right}")
         println("")
     }
 }
