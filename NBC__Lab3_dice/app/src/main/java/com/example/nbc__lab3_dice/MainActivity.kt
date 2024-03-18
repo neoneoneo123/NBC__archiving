2ecfd6fcda1bf1935f49733a67e80a66df31dc94
neo <gouxrunner@gmail.com>
2024-03-18 19:18:31 +0900

    android week3 and project name change

--- NBC__Lab3_dice/app/src/main/java/com/example/nbc__lab3_dice/MainActivity.kt
+++ NBC__Lab3_dice/app/src/main/java/com/example/nbc__lab3_dice/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.nbc__lab3_dice
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
@@ -12,18 +14,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.text_views)
 
-        val tv_num = findViewById<TextView>(R.id.tv_number)
-        val btn_dice = findViewById<Button>(R.id.btn_roll)
+//        val tv_num = findViewById<TextView>(R.id.tv_number)
+//        val btn_dice = findViewById<Button>(R.id.btn_roll)
+//
+//        btn_dice.setOnClickListener {
+//            val random = Random
+//            val num = random.nextInt(6) + 1 //+1을 해야 1부터 시작함
+//
+//            tv_num.text = num.toString()
+//
+//            Log.d("MainActivity", "num = ${num.toString()}")
+//        }
+    }
 
-        btn_dice.setOnClickListener {
-            val random = Random
-            val num = random.nextInt(6) + 1 //+1을 해야 1부터 시작함
-
-            tv_num.text = num.toString()
-
-            Log.d("MainActivity", "num = ${num.toString()}")
-        }
+    fun doAction(v: View) {
+        Toast.makeText(getApplicationContext(), "Submitted Successfully", Toast.LENGTH_SHORT).show()
     }
 }
