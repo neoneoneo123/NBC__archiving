340065b41d04dea1d79afb1475e613f2e733ee39
neo <gouxrunner@gmail.com>
2024-03-21 16:05:31 +0900

    st task week1 phase3

--- NBC__StandardTaskWeek1/app/src/main/java/com/example/nbc__standardtaskweek1/MainActivity.kt
+++ NBC__StandardTaskWeek1/app/src/main/java/com/example/nbc__standardtaskweek1/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.nbc__standardtaskweek1
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.WindowInsets
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        window.insetsController?.hide(WindowInsets.Type.statusBars())
+        val btnSignIn = findViewById<FrameLayout>(R.id.fl_btn_sign_in)
+        btnSignIn.setOnClickListener {
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
         }
     }
 }
