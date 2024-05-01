7ed0579f5c9063884e2fdd2d7dbf8ae7d46ae54d
neoneoneo123 <gouxrunner@gmail.com>
2024-05-01 17:57:37 +0900

    이미지 추가 및 gradle에 라이브러리 추가

--- NBC__dust/app/build.gradle.kts
+++ NBC__dust/app/build.gradle.kts
@@ -37,6 +37,14 @@ android {
 
 dependencies {
 
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    implementation("com.github.skydoves:powerspinner:1.2.6")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
