77b5f4c75b9040d641cacbe51c5b2b9e4e67550c
neoneoneo123 <gouxrunner@gmail.com>
2024-05-01 18:21:52 +0900

    retrofit 사용 부분 작성

---
+++ NBC__dust/app/src/main/java/com/example/nbc__dust/retrofit/NetWorkClient.kt
@@ -0,0 +1,44 @@
+package com.example.nbc__dust.retrofit
+
+import android.util.Config
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object NetWorkClient {
+
+    private const val DUST_BASE_URL = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/"
+
+    /**
+     *http 통신 역할 함수
+     */
+    private fun createOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+
+        if (Config.DEBUG) {
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            interceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+
+        return OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS)
+            .addNetworkInterceptor(interceptor)
+            .build()
+    }
+
+    //이 부분은 약속이므로 이대로 가져가서 써도 됨
+    //생성될 때 url을 넣어서 처리
+    private val dustRetrofit = Retrofit.Builder()
+        .baseUrl(DUST_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create()) //gjon 변환 처리
+        .client(createOkHttpClient())
+        .build()
+
+    //인터페이스를 파라미터로 넣어서 생성
+    val dustNetWork: NetWorkInterface = dustRetrofit.create(NetWorkInterface::class.java)
+}
---
+++ NBC__dust/app/src/main/java/com/example/nbc__dust/retrofit/NetWorkInterface.kt
@@ -0,0 +1,15 @@
+package com.example.nbc__dust.retrofit
+
+import com.example.nbc__dust.data.Dust
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface NetWorkInterface {
+    @GET("getCtprvnRltmMeasureDnsty") //시도별 실시간 측정정보 조회 주소
+
+    /**
+     * 요청 값을 해쉬맵에 넣어서 데이터를 요청하는 함수
+     * Dust 타입으로 반환 받음(GJon 이용)
+     */
+    suspend fun getDust(@QueryMap param: HashMap<String, String>): Dust
+}
