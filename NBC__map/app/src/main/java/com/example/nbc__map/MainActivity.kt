36b844b161319a05ece1c56346c7a14fba6cffae
neoneoneo123 <gouxrunner@gmail.com>
2024-05-01 17:26:18 +0900

    MainActivity 작성. 맵 위치 이동 및 마커 표시

--- NBC__map/app/src/main/java/com/example/nbc__map/MainActivity.kt
+++ NBC__map/app/src/main/java/com/example/nbc__map/MainActivity.kt
@@ -1,20 +1,146 @@
 package com.example.nbc__map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback { //
+
+    private lateinit var mGoogleMap: GoogleMap
+
+    //위치 서비스가 gps를 사용해서 위치를 확인
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    //위치 값 요청에 대한 갱신 정보를 받는 변수
+    lateinit var locationCallback: LocationCallback
+
+    lateinit var locationPermission: ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //권한을 받았는지 확인
+        locationPermission = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { results ->
+
+            if (results.all { it.value }) { //권한이 받아졌으면 onMapReady가 실행됨
+                (supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment)!!.getMapAsync(this)
+            } else {
+                Toast.makeText(this, "권한 승인이 필요합니다.", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        //권한이 없으면 권한 요청
+        locationPermission.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        )
+    }
+
+    //지도 객체를 이용할 수 있는 상황이 될 때
+    override fun onMapReady(p0: GoogleMap) {
+
+        //서울시청에 마커를 추가함
+        val seoul = LatLng(37.566610, 126.978403)
+        mGoogleMap = p0
+        mGoogleMap.mapType = GoogleMap.MAP_TYPE_NORMAL // 기본 노말은 생략 가능
+        mGoogleMap.apply {
+            val markerOptions = MarkerOptions()
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+            markerOptions.position(seoul)
+            markerOptions.title("서울시청")
+            markerOptions.snippet("Tel:01-120")
+            addMarker(markerOptions)
+        }
+
+        //위치 정보를 받아옴
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        updateLocation()
+    }
+
+    /**
+     * 위치를 계속 받아오는 함수
+     */
+    private fun updateLocation() {
+        //위치 요청 변수를 만들어둠
+        val locationRequest = LocationRequest.create().apply {
+            interval = 1000
+            fastestInterval = 500
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        //콜백을 등록해서 초당 한번씩 요청이 들어오도록 함
+        locationCallback = object : LocationCallback() {
+            //1초에 한번씩 변경된 위치 정보가 onLocationResult로 전달된다.
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult?.let {
+                    for (location in it.locations) {
+                        Log.d("위치 정보", "위도: ${location.latitude} 경도: ${location.longitude}")
+                        setLastLocation(location) //현재 위치를 넣어줌
+                    }
+                }
+            }
+        }
+
+        //권한 처리
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        //만든 로케이션과 콜백을 등록시킴
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.myLooper()!!
+        )
+    }
+
+    /**
+     * 위치가 변경되면 호출되는 함수
+     * 지도상의 마커와 카메라를 이동시킴
+     */
+    private fun setLastLocation(lastLocation: Location) {
+        val LATLNG = LatLng(lastLocation.latitude, lastLocation.longitude)
+
+        val makerOptions = MarkerOptions().position(LATLNG).title("나 여기 있어요!")
+        val cameraPosition = CameraPosition.Builder().target(LATLNG).zoom(15.0f).build()
+
+        mGoogleMap.addMarker(makerOptions)
+        mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
 }
