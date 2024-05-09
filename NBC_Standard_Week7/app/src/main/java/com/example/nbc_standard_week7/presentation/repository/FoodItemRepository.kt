626de9aded303ed69ef36b9620ae843f35534a97
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-05-09 14:23:53 +0900

    Revert "Standard week7"

--- NBC_Standard_Week7/app/src/main/java/com/example/nbc_standard_week7/presentation/repository/FoodItemRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nbc_standard_week7.presentation.repository
-
-import com.example.nbc_standard_week7.data.DTO.B553748Items
-import com.example.nbc_standard_week7.presentation.model.FoodItemModel
-
-interface FoodItemRepository {
-    suspend fun b553748Search(pageNo: Int, numOfRows:Int, prdlstNm: String): List<FoodItemModel>
-}
-
