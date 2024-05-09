58cbe074b0f3eeea38e445b3525d564be15f1442
neoneoneo123 <gouxrunner@gmail.com>
2024-05-09 14:19:11 +0900

    gradle, manifest 권한 작성

--- NBC_Standard_Week7/app/build.gradle.kts
+++ NBC_Standard_Week7/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -56,6 +58,20 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-common:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    //optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+    //optional - Test helpers
+    implementation("androidx.room:room-testing:$room_version")
+    annotationProcessor("android.arch.persistence.room:compiler:1.0.0")
+
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
+    kapt("com.github.bumptech.glide:compiler:4.14.2")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
